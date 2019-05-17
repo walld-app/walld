392baf36358a264c76dd2b7521432cc5debbd53c
kz159 <loh@kz159.ru>
2019-05-17 17:56:20 +0300

    added new var, now backgrounds should store in /usr/share/backgrounds/walld

--- walld.py
+++ walld.py
@@ -1,20 +1,24 @@
 #!/bin/python3
-import requests
-import argparse
-import json
-import random
-import os
+import requests, json, random, os, sys
+
 KEY = "25821352e1d420315f04ab8239a625e7"
 API = "https://wall.alphacoders.com/api2.0/get.php"
+MAIN_FOLDER = '/usr/share/backgrounds/walld'
 
 PARAMS = {'auth':KEY, "method":"category",'id':'1',
 'page':str(random.randint(1,5)), 'width': '2160',
 'height':'1080', 'operator':'min'}
 
+if not os.path.exists(MAIN_FOLDER):
+    print("This installation is incorrect! can`t see " + MAIN_FOLDER\
+     + " folder!", file=sys.stderr)
+    exit(1)
+
 def download(url, file_name):
     with open(file_name, "wb") as file:
         response = requests.get(url)
         file.write(response.content)
+
 def set_wall(file_name):
     mon_list = os.popen('xfconf-query -c \
     xfce4-desktop -l | grep "workspace0/last-image"').read().split()
@@ -31,9 +35,9 @@ if r['success']:
         if key >= 95:
             print('ok, key is ', key,'\nPrinting out wall')
             print('downloading', i['url_image'], '...', end = ' ')
-            download(i['url_image'], '/tmp/temp.jpg')
+            download(i['url_image'], MAIN_FOLDER+'/temp.jpg')
             print('ok!')#узнать бы как это более централизованно сделать
-            set_wall('/tmp/temp.jpg')
+            set_wall(MAIN_FOLDER+'/temp.jpg')
             break
         else:
             print('pass, key was ', str(key))
