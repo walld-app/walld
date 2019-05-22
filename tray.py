5570a5dde6911632057ecf609ec2e0b465224f8f
kz159 <loh@kz159.ru>
2019-05-22 23:02:56 +0300

    making resolution changes

--- tray.py
+++ tray.py
@@ -1,8 +1,9 @@
 import PySimpleGUIQt as sg
 
-menu_def = ['BLANK', ['&Open', '---', '&Save', ['1', '2', ['a', 'b']], '&Properties', 'E&xit']]
+menu_def = ['BLANK', ['&Open', '---', '&Save', 'сосать',['хуй','пезду','кок'], ['1', '2', ['a', 'b']],\
+'Resolution', ['1920x1080', '2160x1080'], 'E&xit']]
 
-tray = sg.SystemTray(menu=menu_def, filename=r'default_icon.ico')
+tray = sg.SystemTray(menu=menu_def, filename=r'kk.x-icon')
 
 while True:  # The event loop
     menu_item = tray.Read()
@@ -11,3 +12,5 @@ while True:  # The event loop
         break
     elif menu_item == 'Open':
         sg.Popup('Menu item chosen', menu_item)
+    elif menu_item == '2160x1080':
+        menu_item[1][7][1] = 'kok'
