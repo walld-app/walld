38181f1da8c08e04d0ccff07d1d7e937cc22809c
kz159 <loh@kz159.ru>
2019-06-11 22:10:26 +0300

    moved walld to class now its can be trayed!

--- tray.py
+++ tray.py
@@ -1,23 +1,36 @@
 #!/bin/python3
 import PySimpleGUIQt as sg
+import walld
 
-menu_def = ['BLANK', ['&Open', '---', '&Save', 'сосать',['хуй','пезду','кок'], ['1', '2', ['a', 'b']],\
+menu_def = ['BLANK', ['&Open', '---', 'spin_dice', '&Save', 'сосать',['хуй','пезду','кок'],\
 'Resolution', ['16:9::res', '16:10::res', '21:9::res'], 'E&xit']]
 
-tray = sg.SystemTray(menu=menu_def, filename=r'kk.x-icon')
+walld = walld.Walld()
+
+tray = sg.SystemTray(menu=menu_def, filename=r'temp/kk.x-icon')
 def tray_start():
     while True:  # The event loop
         menu_item = tray.Read()
         print(menu_item)
         if menu_item == 'Exit':
             break
+
         elif menu_item == 'Open':
             sg.Popup('Menu item chosen', menu_item)
+
         elif '*' in menu_item: # ВОПРОС А ЕСЛИ МЫ ПОЛУЧИМ ДРУГУЮ КАТЕГОРИЮ? НУЖНО СДЕЛАТЬ ПОИС ЧУТЬ ГЛОБАЛЬНЕЙ
         #ТУТ НУЖЕН ЦИКЛ FOR ДЛЯ ПРОШЕРСТЕНИЯ ВСЕХ ЛИСТОВ
             print('ok')
             menu_def[1][7][menu_def[1][7].index(menu_item)] = menu_item[1:]
             tray.Update(menu=menu_def)
+
         elif 'res' in menu_item:
             menu_def[1][7][menu_def[1][7].index(menu_item)] = '*' + menu_item
             tray.Update(menu=menu_def)
+
+        elif menu_item == 'spin_dice':
+            walld.spin_dice(95)
+
+        elif menu_item == 'Save':
+            print(walld.save_image())
+tray_start()
--- walld.py
+++ walld.py
@@ -1,12 +1,12 @@
 #!/bin/python3
-import requests, json, random, os, sys, pickle
+import requests, json, random, os, sys, pickle, logging
 
 KEY = "25821352e1d420315f04ab8239a625e7"
 API = "https://wall.alphacoders.com/api2.0/get.php"
 MAIN_FOLDER = '/usr/share/backgrounds/walld'
 
 PARAMS = {'auth':KEY, "method":"category",'id':'1',
-'page':str(random.randint(1,5)), 'width': '2160',
+'page':str(random.randint(1,10)), 'width': '2160',
 'height':'1080', 'operator':'min'}
 
 if not os.path.exists(MAIN_FOLDER):
@@ -14,37 +14,60 @@ if not os.path.exists(MAIN_FOLDER):
      + " folder!", file=sys.stderr)
     exit(1)
 
-def dprint(text):
-    if DEBUG:
-        print(text)
 
-class
-def download(url, file_name):
-    with open(file_name, "wb") as file:
-        response = requests.get(url)
-        file.write(response.content)
+class Walld(object):
+    DEBUG = 1
+    '''this class represents all walld functions except trays one'''
 
-def set_wall(file_name):
-    mon_list = os.popen('xfconf-query -c \
-    xfce4-desktop -l | grep "workspace0/last-image"').read().split()
-    for i in mon_list:
-        print(i)
-        os.system('xfconf-query \
-        --channel xfce4-desktop --property '+ i +' --set ' + file_name)
+    def __init__(self):
+        self.dprint('class walld started!')
 
-r = json.loads(requests.get(API, params=PARAMS).text)
-if r['success']:
-    #print(r['wallpapers'])
-    for i in r['wallpapers']:
-        key = random.randint(1, 100)
-        if key >= 95:
-            print('ok, key is ', key,'\nPrinting out wall')
-            print('downloading', i['url_image'], '...', end = ' ')
-            download(i['url_image'], MAIN_FOLDER+'/temp.jpg')
-            print('ok!')#узнать бы как это более централизованно сделать
-            set_wall(MAIN_FOLDER+'/temp.jpg')
-            break
+    def dprint(self,text): #найти дебаггер?
+        if self.DEBUG:
+            print(text)
+
+    def download(self,url, file_name):
+        with open(file_name, "wb") as file:
+            response = requests.get(url)
+            file.write(response.content)
+
+    def set_wall(self,file_name):
+        mon_list = os.popen('xfconf-query -c \
+        xfce4-desktop -l | grep "workspace0/last-image"').read().split()
+        for i in mon_list:
+            print(i)
+            os.system('xfconf-query \
+            --channel xfce4-desktop --property '+ i +' --set ' + file_name)
+
+    def current_image(self, store = False):
+        #сохранить бы в файл если честно
+        if store:
+            self.var = store
         else:
-            print('pass, key was ', str(key))
-else:
-    print('wops!')
+            return self.var
+
+    def save_image(self):
+        self.save_path =  MAIN_FOLDER+'/saved/' + str(random.random())
+        os.system('cp ' + MAIN_FOLDER+'/temp.jpg ' + MAIN_FOLDER + '/saved/' + str(random.random()))# имя бы достать
+        return 'saved at ' + self.save_path
+
+    def guess_de(self):
+        pass#need to guess current de
+
+    def spin_dice(self, chance):
+        r = json.loads(requests.get(API, params=PARAMS).text)
+        if r['success']:
+            #print(r['wallpapers'])
+            for i in r['wallpapers']:
+                key = random.randint(0, 100)# IDEA: ЧО ЕСЛИ ВЗЯТЬ ВЕСЬ СПИСОК И ТОЛЬКО ПО НЕМУ УЖЕ ХУЯЯЧИТЬ ТОЕСТЬ РАСПАРСИТЬ ВСЕ СТРАНИЧКИ И ЕБАШИТЬ УЖЕ ПО НЕМУ ПРОЦЕНТАМИ
+                if key >= chance:
+                    print('ok, key is ' + str(key) +'\nPrinting out wall')
+                    print('downloading', i['url_image'], '...', end = ' ')
+                    self.download(i['url_image'], MAIN_FOLDER+'/temp.jpg')
+                    print('ok!')#узнать бы как это более централизованно сделать
+                    self.set_wall(MAIN_FOLDER+'/temp.jpg')
+                    break
+                else:
+                    print('pass, key was '+ str(key))
+        else:
+            print('wops!')
