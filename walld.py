1937c0abf3f4b5987aff1e41d8d9b5c72da9da55
kz159 <loh@kz159.ru>
2019-06-12 14:05:17 +0300

    polishing

--- walld.py
+++ walld.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-import requests, json, random, os, sys, pickle, logging
+import requests, json, random, os, sys, logging
 
 KEY = "25821352e1d420315f04ab8239a625e7"
 API = "https://wall.alphacoders.com/api2.0/get.php"
@@ -14,6 +14,18 @@ if not os.path.exists(MAIN_FOLDER):
      + " folder!", file=sys.stderr)
     exit(1)
 
+def download(url, file_name):
+    with open(file_name, "wb") as file:
+        response = requests.get(url)
+        file.write(response.content)
+
+def set_wall(file_name):
+    mon_list = os.popen('/usr/bin/xfconf-query -c \
+    xfce4-desktop -l | grep "workspace0/last-image"').read().split()
+    for i in mon_list:
+        print(i)
+        os.system('xfconf-query \
+        --channel xfce4-desktop --property '+ i +' --set ' + file_name)
 
 class Walld(object):
     DEBUG = 1
@@ -21,23 +33,6 @@ class Walld(object):
 
     def __init__(self):
         self.dprint('class walld started!')
-
-    def dprint(self,text): #найти дебаггер?
-        if self.DEBUG:
-            print(text)
-
-    def download(self,url, file_name):
-        with open(file_name, "wb") as file:
-            response = requests.get(url)
-            file.write(response.content)
-
-    def set_wall(self,file_name):
-        mon_list = os.popen('xfconf-query -c \
-        xfce4-desktop -l | grep "workspace0/last-image"').read().split()
-        for i in mon_list:
-            print(i)
-            os.system('xfconf-query \
-            --channel xfce4-desktop --property '+ i +' --set ' + file_name)
 
     def current_image(self, store = False):
         #сохранить бы в файл если честно
@@ -63,9 +58,9 @@ class Walld(object):
                 if key >= chance:
                     print('ok, key is ' + str(key) +'\nPrinting out wall')
                     print('downloading', i['url_image'], '...', end = ' ')
-                    self.download(i['url_image'], MAIN_FOLDER+'/temp.jpg')
+                    download(i['url_image'], MAIN_FOLDER+'/temp.jpg')
                     print('ok!')#узнать бы как это более централизованно сделать
-                    self.set_wall(MAIN_FOLDER+'/temp.jpg')
+                    set_wall(MAIN_FOLDER+'/temp.jpg')
                     break
                 else:
                     print('pass, key was '+ str(key))
