9160441be70fa20ee17666caef2c56225e281e14
auto kz159 <auto-loh@kz159.ru>
2019-08-01 11:26:34 +0100

    added changes to config to close some issues #2 and #3

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 KEY = "25821352e1d420315f04ab8239a625e7"
 API = "https://wall.alphacoders.com/api2.0/get.php"
-MAIN_FOLDER = '/usr/share/backgrounds/walld'
-DB_URL = 'walld.net/stuff/tt.db'
+MAIN_FOLDER = '~/.walld'
+DB_URL = 'http://walld.net/stuff/tt.db'
 DB_NAME = 'db.db'
-SETTINGS_FILE = 'temp/settings.conf'
+SETTINGS_FILE = MAIN_FOLDER +'/settings.conf'
