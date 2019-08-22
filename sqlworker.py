b5dcd368ed2467a5f4a7c767dc29e9835169d5b1
kz159 <loh@kz159.ru>
2019-08-22 10:59:12 +0300

    bringing down dead code

--- sqlworker.py
+++
@@ -1,5 +0,0 @@
-#            print('making new db')
-#            cursor.execute("""CREATE TABLE pics
-#            (id integer, category text, sub_category text, url text,
-#            how_many_used integer, likes integer, extra text)
-#            """)
