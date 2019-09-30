76a0dcf25e77d16ab4d4fa2b79c56a24ce9d52c3
kz159 <loh@kz159.ru>
2019-10-01 00:15:39 +0300

    code style, and bug fix

--- core.py
+++ core.py
@@ -17,26 +17,32 @@ class Walld():
         self.api = api
         self.save_path = self.main_folder+'/saved/' + str(random.random()) + '.png'#nosec
         self.main_folder_temp = self.main_folder + '/temp.jpg'
+
         if  platform.system() == 'Windows' : #here comes windows specific stuff
             self.desktop_environment = platform.system()
+        
         else:
             code = "/usr/bin/env | /usr/bin/grep DESKTOP_SESSION= \
             | /usr/bin/awk -F= '{print $2}'"
             self.desktop_environment = \
             subprocess.check_output(code, shell=True).decode('ascii')#nosec, redo
+        
         if not os.path.exists(self.main_folder):
             print("can`t see "\
             + self.main_folder + " folder!")
             exit(1)
+        
         print('class walld started!')
 
     def save_image(self, name=False):
         '''copy image to specific(if passed) folder or to standart
         self.save_path path'''
         print(self.save_path)
+        
         if name:
             shutil.copyfile(self.main_folder_temp, name)#nosec
             print('saved at ' + name)
+        
         else:
             shutil.copyfile(self.main_folder_temp,\
              self.save_path)#nosec wont fix
@@ -46,9 +52,12 @@ class Walld():
     def spin_dice(self):
         '''making a list of urls by accessing a db, than sets wall'''
         new_url = self.get_urls()['url']
+        
         if new_url == (404 or 403):
             print('API IS NOT RESPONDING CORRECTLY') # tell this to user
+        
         else:# api on that state is working BUT is this url good?
+        
             if check_url(new_url):
                 self.set_wall(download(new_url,\
                 self.main_folder+'/temp.jpg'))
@@ -60,26 +69,32 @@ class Walld():
         '''this is critical module, depending on de it sets walls'''
         if self.desktop_environment == 'xfce\n':
             mon_list = subprocess.check_output('/usr/bin/xfconf-query -c \
-xfce4-desktop -l | grep "workspace0/last-image"', shell=True).split()#nosec, rewrite
-            print(mon_list)
+                                                xfce4-desktop -l | grep \
+                                                "workspace0/last-image"',
+                                                 shell=True).split()#nosec, rewrite
             for i in mon_list:
                 subprocess.call(['/usr/bin/xfconf-query',#nosec
                                  '--channel', 'xfce4-desktop', '--property',
                                  i, '--set', file_name])
-        elif self.desktop_environment == 'mate\n' or 'lightdm-xsession': #experimental
+
+        elif self.desktop_environment == ('mate\n' or 'lightdm-xsession'): #experimental
             subprocess.run(['/usr/bin/gsettings', 'set',#nosec wont fix
                             'org.mate.background', 'picture-filename',
                             file_name])
+
         elif self.desktop_environment == 'gnome\n': #experimental
             subprocess.run(['/usr/bin/gsettings', 'set',#nosec wont fix
                             'org.gnome.desktop.background',
                             'picture-uri', '"file://' + file_name + '"'])
+
         elif self.desktop_environment == 'cinnamon2d\n':
              subprocess.run(['/usr/bin/gsettings', 'set',#nosec wont fix
                             'org.cinnamon.desktop.background',
                             'picture-uri', '"file://' + file_name + '"'])
+
         elif self.desktop_environment == 'i3\n':
             subprocess.run(['/usr/bin/feh', '--bg-scale', file_name])
+
         elif self.desktop_environment == 'Windows':
             # this is windows specific stuff, here we update our "online" wallpaper
             ctypes.windll.user32.SystemParametersInfoW(20, 0, file_name, 0)
@@ -96,20 +111,24 @@ xfce4-desktop -l | grep "workspace0/last-image"', shell=True).split()#nosec, rew
     def get_urls(self):
         '''requests new link for wallpaper'''
         params = []
+
         if self.filer.settings['categories']:
             print(self.filer.settings['categories'])
             cat = random.choice(list(self.filer.settings['categories'].keys()))#nosec
             sub_cat = random.choice(self.filer.settings['categories'][cat])#nosec
             params.append(("category", cat))
             params.append(('sub_category', sub_cat))
+        
         if not params:
             params = [('random', '1')]
         answer = requests.get(self.api\
          + '/walls', params=params)
         json_answer = json.loads(answer.text)
+        
         if json_answer['success']:
             print(params)
             result = json_answer['content']
+        
         else:
             print('here the params', params)
             if answer.status_code == '404':
@@ -128,6 +147,7 @@ xfce4-desktop -l | grep "workspace0/last-image"', shell=True).split()#nosec, rew
         params = {"param":"categories"}
         json_answer = json.loads(requests.get(self.api, params=params).text)
         ong = []
+        
         for i in json_answer['content']:
             ong.append(i['category']+'::cat_')
             ong.append([l+'::sca_::'+ i['category']  for l in i['subs']])
@@ -138,13 +158,17 @@ class Filer():
     def __init__(self, main_folder):
         self.main_folder = main_folder
         self.settings_file = self.main_folder + '/prefs.json'
+        
         if not os.path.exists(self.main_folder):
             print('creating!' + self.main_folder)
             os.mkdir(self.main_folder)
+        
         if not os.path.exists(self.main_folder+'/saved/'):
             print('creating!' + self.main_folder+'/saved/')
             os.mkdir(self.main_folder+'/saved/')
+        
         print('checking options!')
+        
         try:
             with open(self.settings_file, 'r') as file:
                 self.settings = json.load(file)
@@ -157,27 +181,31 @@ class Filer():
         '''works with options file'''
         if add:
             print('adding', name)
-            #if 'cat_' in name:
-            #    self.settings['categories'][name] = {}
             if 'res_' in name:
                 self.settings['resolutions'].append(name)
+        
             elif 'sca_' in name:
                 if not name.split('::')[2] in self.settings['categories']:
                     self.settings['categories'][name.split('::')[2]] = []
                 self.settings['categories'][name.split('::')[2]].append(name.split('::')[0])
+        
         else:
             print('removing', name)
             if 'cat_' in name:
                 self.settings['categories'].remove(name[1:])
+
             elif 'res_' in name:
                 self.settings['resolutions'].remove(name[1:])
+
             elif 'sca_' in name:
                 lst = name.split("::")
                 self.settings['categories'][lst[2]].remove(lst[0][1:])
+
         del_list = []
         for i in self.settings['categories']:
             if not self.settings['categories'][i]:
                 del_list.append(i)
+
         for i in del_list:
             del self.settings['categories'][i]
         self.dump()
