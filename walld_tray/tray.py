1a530b1a9b1853a1832a89cd6ab213ba86404099
kz159 <loh@kz159.ru>
2020-08-31 10:00:16 +0300

    renaming, cuttin` legacy

--- walld_tray/tray.py
+++ walld_tray/tray.py
@@ -1,106 +1,122 @@
-#!/bin/python3
-"""
-Main script that sits in walld_tray
-and calls walld for help
-PYSIMPLEGUIQT VERSION
-OUTDATED!!
-"""
-import argparse
 import sys
+from functools import partial
+from pathlib import Path
+from typing import Any, Union
 
-from PySimpleGUIQt import PopupGetFile, SystemTray
+from PyQt5 import QtGui, uic
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import (QAction, QApplication, QMainWindow, QMenu, QPushButton, QSizePolicy, QSpacerItem,
+                             QSystemTrayIcon, QWidget
+                             )
 
-from walld_tray.config import API
-from walld_tray.core import Walld
+from config import API, ICON
+from core import Walld
+from helpers import b64_to_icon, clear_layout
 
-walld = Walld(API)
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', action='store_true', help='change wallpaper')
-args = parser.parse_args()
-
-if args.c:
-    print(args.c)
-    walld.set_wall_from_api()
-    sys.exit()
-
-#'Resolution', ['16:9::res_', '16:10::res_', '21:9::res_']
-menu_def = ['BLANK', ['Change wallpaper', '---', '&Save',
-                      'Save as...', 'Category',
-                      walld.get_categories(), 'E&xit', '!master']]
-
-TRAY = SystemTray(menu=menu_def, data_base64=config.ICON)
-
-def make_flip(item): # сразу лезть в файл настроек и там все менять?
-    '''operates with settings and updates walld_tray with dots'''
-    # далее вызывать tray_update()
-    if 'sca_' in item:
-        place = 5
-
-    elif "res_" in item:
-        place = 7
-
-    var = item.split('::')[2]+'::cat_'
-    second = menu_def[1][place].index(var) + 1
-    last = (menu_def[1][place][second].index(item))
-
-    if '*' in item:
-        if 'sca_' in item:
-            menu_def[1][place][second][last] = item[1:] 
-            #this is for sub cat if * in it
-        elif "res_" in item:
-            menu_def[1][place][menu_def[1][place].index(item)] = item[1:]
-        #menu_def[1][place][menu_def[1][place].index(item)] = item[1:]
-        #dont touch it dumbass
-        walld.change_option(item)
-
-    else:
-        if 'sca_' in item:
-            menu_def[1][place][second][last] = '*' + item
-        elif "res_" in item:
-            menu_def[1][place][menu_def[1][place].index(item)] = '*' + item
-        walld.change_option(item, add=True)
-    TRAY.Update(menu=menu_def)
+TEMPLATE_DIR: Union[Path, Any] = Path('.') / "template"
 
 
-def restore_settings():
-    '''restores settings on startup'''
-    for i in walld.get_settings()['categories']:
-        for value in walld.get_settings()['categories'][i]:
-            nibba = value +'::sca_::' +i
-            try:
-                second = menu_def[1][5].index(i+'::cat_') +1
-            except ValueError:
+class CategoryWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(TEMPLATE_DIR / "category_widget.ui", self)
+
+
+class Ui(QMainWindow):
+    def __init__(self, walld, icon: QtGui.QIcon):
+        super().__init__()
+        uic.loadUi(TEMPLATE_DIR / "settings.ui", self)  # Load the .ui file
+        #  self.show()  # Show the GUI
+        self.category_widget = CategoryWidget()
+        self.walld = walld  # TODO it will crash if walld is not connected to a service
+        self.icon = icon
+        self.tray = QSystemTrayIcon()
+        self.tray.setIcon(self.icon)
+        self.tray.setVisible(True)
+        self._gen_categories_buttons()
+        self.RightMenu.addWidget(self.category_widget)
+        self.category_widget.hide()
+
+    def _gen_categories_buttons(self):
+        self.cats_buttons = {}
+        spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        for i in self.walld.categories:
+            ll = QPushButton(i)
+            ll.setMinimumSize(QSize(0, 40))
+            self.cats_buttons[i] = dict(category=ll, sub_categories=[])
+            # make for loop for iterating over sub category
+            for sub_category in self.walld.categories[i]:
+                sub_category_button = QPushButton(sub_category['name'])
+                sub_category_button.setMinimumSize(QSize(0, 40))
+                sub_category_button.setCheckable(True)
+                sub_category_button.setChecked(sub_category['checked'])
+                self.cats_buttons[i]['sub_categories'].append(sub_category_button)
+                self.category_widget.SubCategoriesLayout.addWidget(sub_category_button)
+                sub_category_button.hide()
+            self.category_widget.CategoriesLayout.addWidget(ll)
+        self.category_widget.SubCategoriesLayout.addSpacerItem(spacer)
+        self.category_widget.CategoriesLayout.addSpacerItem(spacer)
+
+    def bring_sub_categories(self, category):
+        self.hide_buttons('sub_categories')
+        for i in self.cats_buttons[category]['sub_categories']:
+            i.show()
+
+    def hide_buttons(self, category):
+        for button in self.cats_buttons:
+            for i in self.cats_buttons[button].get(category, []):
+                i.hide()
+
+    def check_button(self, category, button):  # TODO REDO
+        for num, i in enumerate(self.walld.prefs_in_mem['categories'][category]):
+            if i['name'] == button.text():
+                self.walld.prefs_in_mem['categories'][category][num]['checked'] = button.isChecked()
                 break
-            last = (menu_def[1][5][second].index(nibba))
-            menu_def[1][5][second][last] = "*" + nibba
-    for i in walld.get_settings()['resolutions']:
-        menu_def[1][7][menu_def[1][7].index(i)] = "*" + i
-    TRAY.Update(menu=menu_def)
 
-def tray_start():
-    '''main funcrion, starts walld_tray'''
-    restore_settings()
-    while True:  # The event loop
-        menu_item = TRAY.Read()
-        print(menu_item)
-        if menu_item == 'Exit':
-            break
+        self.walld.prefs = self.walld.prefs_in_mem
 
-        elif menu_item == 'Save as...':
-            apath = PopupGetFile('hi',
-                                 save_as=True,
-                                 file_types=(('PNG files', '*.png'),
-                                             ('JPEG files', '*.jpg')))
-            walld.save_image(apath)
 
-        elif menu_item in ('__ACTIVATED__', 'Change wallpaper'):
-            walld.set_wall_from_api()
+class UiCtrl:
+    """Ui Controller class."""
+    def __init__(self, view):
+        """Controller initializer."""
+        self.view = view
+        # Connect signals and slots
+        self._connect_signals()
 
-        elif ('cat_' in menu_item or 'res_' in menu_item or 'sca_' in menu_item):
-            print('aha')
-            make_flip(menu_item)
+    def _connect_signals(self):
+        """Connect signals and slots."""
+        self.view.ColourButton.clicked.connect(self.view.category_widget.show)
+        self.view.CategoriesButton.clicked.connect(self.view.category_widget.show)
+        self.view.TagButton.clicked.connect(partial(clear_layout, self.view.RightMenu))
 
-        elif menu_item == 'Save':
-            walld.save_image()
+        for i in self.view.cats_buttons:
+            button = self.view.cats_buttons[i]['category']
+            button.clicked.connect(partial(self.view.bring_sub_categories, button.text()))
+            sub_categories_buttons = self.view.cats_buttons[i]['sub_categories']
+            for sc_button in sub_categories_buttons:
+                sc_button.clicked.connect(partial(self.view.check_button, button.text(), sc_button))
 
-tray_start()
+
+def main():
+    walld_core = Walld(API)  # TODO Exception for not connecting
+    app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    window = Ui(walld_core, b64_to_icon(ICON))
+    UiCtrl(window)
+
+    menu = QMenu()
+    shut_down = QAction("Quit")
+    shut_down.triggered.connect(app.quit)
+
+    change_wallpaper = QAction("Change wallpaper")
+    change_wallpaper.triggered.connect(walld_core.set_wall)
+    settings = QAction("Settings")
+    settings.triggered.connect(window.show)
+
+    menu.addAction(change_wallpaper)
+    menu.addAction(settings)
+    menu.addAction(shut_down)
+    window.tray.setContextMenu(menu)
+
+    sys.exit(app.exec_())
---
+++ walld_tray/tray_legacy.py
@@ -0,0 +1,106 @@
+#!/bin/python3
+"""
+Main script that sits in walld_tray
+and calls walld for help
+PYSIMPLEGUIQT VERSION
+OUTDATED!!
+"""
+import argparse
+import sys
+
+from PySimpleGUIQt import PopupGetFile, SystemTray
+
+from walld_tray.config import API
+from walld_tray.core import Walld
+
+walld = Walld(API)
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', action='store_true', help='change wallpaper')
+args = parser.parse_args()
+
+if args.c:
+    print(args.c)
+    walld.set_wall_from_api()
+    sys.exit()
+
+#'Resolution', ['16:9::res_', '16:10::res_', '21:9::res_']
+menu_def = ['BLANK', ['Change wallpaper', '---', '&Save',
+                      'Save as...', 'Category',
+                      walld.get_categories(), 'E&xit', '!master']]
+
+TRAY = SystemTray(menu=menu_def, data_base64=config.ICON)
+
+def make_flip(item): # сразу лезть в файл настроек и там все менять?
+    '''operates with settings and updates walld_tray with dots'''
+    # далее вызывать tray_update()
+    if 'sca_' in item:
+        place = 5
+
+    elif "res_" in item:
+        place = 7
+
+    var = item.split('::')[2]+'::cat_'
+    second = menu_def[1][place].index(var) + 1
+    last = (menu_def[1][place][second].index(item))
+
+    if '*' in item:
+        if 'sca_' in item:
+            menu_def[1][place][second][last] = item[1:] 
+            #this is for sub cat if * in it
+        elif "res_" in item:
+            menu_def[1][place][menu_def[1][place].index(item)] = item[1:]
+        #menu_def[1][place][menu_def[1][place].index(item)] = item[1:]
+        #dont touch it dumbass
+        walld.change_option(item)
+
+    else:
+        if 'sca_' in item:
+            menu_def[1][place][second][last] = '*' + item
+        elif "res_" in item:
+            menu_def[1][place][menu_def[1][place].index(item)] = '*' + item
+        walld.change_option(item, add=True)
+    TRAY.Update(menu=menu_def)
+
+
+def restore_settings():
+    '''restores settings on startup'''
+    for i in walld.get_settings()['categories']:
+        for value in walld.get_settings()['categories'][i]:
+            nibba = value +'::sca_::' +i
+            try:
+                second = menu_def[1][5].index(i+'::cat_') +1
+            except ValueError:
+                break
+            last = (menu_def[1][5][second].index(nibba))
+            menu_def[1][5][second][last] = "*" + nibba
+    for i in walld.get_settings()['resolutions']:
+        menu_def[1][7][menu_def[1][7].index(i)] = "*" + i
+    TRAY.Update(menu=menu_def)
+
+def tray_start():
+    '''main funcrion, starts walld_tray'''
+    restore_settings()
+    while True:  # The event loop
+        menu_item = TRAY.Read()
+        print(menu_item)
+        if menu_item == 'Exit':
+            break
+
+        elif menu_item == 'Save as...':
+            apath = PopupGetFile('hi',
+                                 save_as=True,
+                                 file_types=(('PNG files', '*.png'),
+                                             ('JPEG files', '*.jpg')))
+            walld.save_image(apath)
+
+        elif menu_item in ('__ACTIVATED__', 'Change wallpaper'):
+            walld.set_wall_from_api()
+
+        elif ('cat_' in menu_item or 'res_' in menu_item or 'sca_' in menu_item):
+            print('aha')
+            make_flip(menu_item)
+
+        elif menu_item == 'Save':
+            walld.save_image()
+
+tray_start()
